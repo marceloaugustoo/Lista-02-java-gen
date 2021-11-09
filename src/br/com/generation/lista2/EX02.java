77268319912cf6ede1105e2dbb9de650c8732c67
Marcelo Augusto <93771365+marceloaugustoo@users.noreply.github.com>
2021-11-09 15:20:38 -0300

    Update EX02.java

--- src/br/com/generation/lista2/EX02.java
+++ src/br/com/generation/lista2/EX02.java
@@ -7,32 +7,36 @@ public class EX02 {
 		int num1,num2,num3;
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Digite três numeros para colocar em ordem crescente.");
+		System.out.println("Digite trÃªs numeros para colocar em ordem crescente.");
 		
 		num1 = scan.nextInt();
 		num2 = scan.nextInt();
 		num3 = scan.nextInt();
 		
-		if(num1>num2 && num1>num3 && num2>num3) {
+		if(num1>=num2 && num1>=num3 && num2>=num3) {
 			System.out.printf("Ordem crescente: %d,%d,%d",num3,num2,num1);
 		}
-		if(num1>num2 && num1>num3 && num3>num2) {
+		else if(num1>=num2 && num1>=num3 && num3>=num2) {
 			System.out.printf("Ordem crescente: %d,%d,%d",num2,num3,num1);
 		}
-		if(num2>num1 && num2>num3 && num1>num3) {
+		else if(num2>=num1 && num2>=num3 && num1>=num3) {
 			System.out.printf("Ordem crescente: %d,%d,%d",num3,num1,num2);
 		}
-		if(num2>num1 && num2>num3 && num3>num1) {
+		else if(num2>=num1 && num2>=num3 && num3>=num1) {
 			System.out.printf("Ordem crescente: %d,%d,%d",num1,num3,num2);
 		}
-		if(num3>num2 && num3>num1 && num2>num1) {
+		else if(num3>=num2 && num3>=num1 && num2>=num1) {
 			System.out.printf("Ordem crescente: %d,%d,%d",num1,num2,num3);
 		}
-		if(num3>num2 && num3>num1 && num1>num2) {
+		else if(num3>=num2 && num3>=num1 && num1>=num2) {
 			System.out.printf("Ordem crescente: %d,%d,%d",num2,num1,num3);
 		}
 		
 		scan.close();
+	}
+
+}
+
 	}
 
 }
